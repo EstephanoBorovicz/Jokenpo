c40393096201291f95233308cf82f1a7f3c0b074
EstephanoBorovicz <estephano_borovicz@outlook.com>
2025-10-06 15:35:21 -0300

    otimização: substituição das variáveis em texto por impressão condicional

--- src/src/model/PedraPapelTesoura.java
+++ src/src/model/PedraPapelTesoura.java
@@ -5,10 +5,7 @@ import java.util.Random;
 public class PedraPapelTesoura {
     public int escolhaDoUsuario;
     public int escolhaDoComputador;
-    public String escolhaDoUsuarioEmTexto;
-    public String escolhaDoComputadorEmTexto;
     public String resultadoDaPartida;
-    public int respostaDoUsuarioSobreJogarDeNovo;
     Scanner leitor = new Scanner(System.in);
 
     public void GetPedraPapelTesoura(){
@@ -28,22 +25,22 @@ public class PedraPapelTesoura {
     public void RandomizarEscolhaDoComputador(){
         Random numeroAleatorio = new Random();
         escolhaDoComputador = numeroAleatorio.nextInt(3)+1;
-        //o intervalo de escolha é [0,n[, portanto a escolha do computador precisa ser de 0 a 3 (0,1,2) + 1
+        //o intervalo de escolha é [0,n[, portanto a escolha do computador precisa ser de 0 a 3 (0,1,2) + 1 = (1,2,3)
         FormularLogicaDoJogo();
     }
 
     public void FormularLogicaDoJogo() {
         if (escolhaDoUsuario==1 && escolhaDoComputador== 2){
             resultadoDaPartida = "Você perdeu :(";
-        }else if (escolhaDoUsuario==1 && escolhaDoComputador== 3){
+        }else if(escolhaDoUsuario==1 && escolhaDoComputador== 3){
             resultadoDaPartida = "*** V O C Ê  V E N C E U ! ! ! ***";
         }else if(escolhaDoUsuario==2 && escolhaDoComputador==1){
             resultadoDaPartida = "*** V O C Ê  V E N C E U ! ! ! ***";
-        }else if (escolhaDoUsuario==2 && escolhaDoComputador==3){
+        }else if(escolhaDoUsuario==2 && escolhaDoComputador==3){
             resultadoDaPartida = "Você perdeu :(";
         }else if(escolhaDoUsuario==3 && escolhaDoComputador==1){
             resultadoDaPartida = "Você perdeu :(";
-        }else if (escolhaDoUsuario==3 && escolhaDoComputador==2){
+        }else if(escolhaDoUsuario==3 && escolhaDoComputador==2){
             resultadoDaPartida = "*** V O C Ê  V E N C E U ! ! ! ***";
         }else{
             resultadoDaPartida = "Empate!";
@@ -53,29 +50,29 @@ public class PedraPapelTesoura {
     public void ExibirResultado(){
 
         if (escolhaDoUsuario==1) {
-            escolhaDoUsuarioEmTexto = "Pedra";
+            System.out.println("Você escolheu: pedra");
         } else if (escolhaDoUsuario==2) {
-            escolhaDoUsuarioEmTexto = "Papel";
+            System.out.println("Você escolheu: papel");
         } else if (escolhaDoUsuario==3) {
-            escolhaDoUsuarioEmTexto = "Tesoura";
+            System.out.println("Você escolheu: tesoura");
         }
 
         if (escolhaDoComputador==1) {
-            escolhaDoComputadorEmTexto = "Pedra";
+            System.out.println("O computador escolheu: pedra");
         }else if(escolhaDoComputador==2) {
-            escolhaDoComputadorEmTexto = "Papel";
-        }else if(escolhaDoComputador==3){
-            escolhaDoComputadorEmTexto = "Tesoura";}
-
-        System.out.println("Você escolheu: " + escolhaDoUsuarioEmTexto);
-        System.out.println("O computador escolheu: " + escolhaDoComputadorEmTexto);
+            System.out.println("O computador escolheu: papel");
+        }else if(escolhaDoComputador==3) {
+            System.out.println("O computador escolheu: tesoura");
+        }
         System.out.println(resultadoDaPartida);
         JogarDeNovo();
     }
     public void JogarDeNovo(){
-        System.out.print("Jogar de novo? (Sim = 1, não = 0) ");
-        respostaDoUsuarioSobreJogarDeNovo = leitor.nextInt();
-        if (respostaDoUsuarioSobreJogarDeNovo == 1) GetPedraPapelTesoura();
-        else System.exit(0);
+        System.out.println("Jogar de novo? (Sim = 1, não = 0) ");
+        if (leitor.nextInt() == 1) {
+            GetPedraPapelTesoura();
+        } else {
+            System.exit(0);
+        }
     }
 }
